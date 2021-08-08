73849a1208ff97cf5eb7a191aa2848d92b3d8102
Lucas Solderagit config --global user.name Lucas <lucas.s.soldera2004@gmail.com>
2021-08-08 11:00:17 -0300

    backup e confirmação via e-mail

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command('backup:run')->dailyAt('00:00', '12:00');
     }
 
     /**
