a7a103b60f1616ea41687b4fa5d0dfcc4bfe98df
Lucas Solderagit config --global user.name Lucas <lucas.s.soldera2004@gmail.com>
2021-09-18 15:22:30 -0300

    Validar se o usuário está logado

--- app/Http/Controllers/Delete/UserDelete.php
+++ app/Http/Controllers/Delete/UserDelete.php
@@ -6,17 +6,24 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 use App\Models\Usuario;
+use Illuminate\Support\Facades\Auth;
 
 class UserDelete extends Controller
 {
     public function deleteUser(Request $request)
     {
+
         $data=Usuario::find($request->id);
+
+        if(Auth::user() == $data->usu_usuario){
+            return response()->json(['msg' => 'Impossível excluir, usuário em uso!']);
+        } else{
 
         $nome = $data->usu_nome_completo;
 
         $data->delete();
         $msgExcluir = "O usuário $nome foi excluído com sucesso!";
         return response()->json(['msg' => $msgExcluir]);
+        }
     }
 }
