d5ad6345c43038247f7f67f0ed008f24347494f5
Lucas Solderagit config --global user.name Lucas <lucas.s.soldera2004@gmail.com>
2021-10-05 21:46:20 -0300

    Ícone da akert de delete corrigido

--- resources/views/dashboard/clientes.blade.php
+++ resources/views/dashboard/clientes.blade.php
@@ -590,7 +590,7 @@ $.ajax({
     success: function(data_decoded) {
             $('#formExcluir')[0].reset();
             $('#modalAlertDelete').hide();
-            demo.showNotification('top','right',4,data_decoded.msg, 'icon-alert-circle-exc');
+            demo.showNotification('top','right',4,data_decoded.msg, 'tim-icons icon-alert-circle-exc');
     }
 });
 });
