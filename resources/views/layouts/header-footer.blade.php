432bbd7ee74364ee059f40b08511143e74ac493a
Lucas Solderagit config --global user.name Lucas <lucas.s.soldera2004@gmail.com>
2021-11-04 23:45:31 -0300

    CDNs atualizados

--- resources/views/layouts/header-footer.blade.php
+++ resources/views/layouts/header-footer.blade.php
@@ -35,6 +35,7 @@
     <!-- CSS Just for demo purpose, don't include it in your project -->
     <link href="../demo/demo.css" rel="stylesheet" />
 
+
     <!--   Core JS Files   -->
     <script src="../js/dash/core/jquery.min.js"></script>
     <script src="../js/dash/core/popper.min.js"></script>
@@ -54,9 +55,12 @@
         integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-typeahead/2.11.0/jquery.typeahead.min.js" integrity="sha512-Rc24PGD2NTEGNYG/EMB+jcFpAltU9svgPcG/73l1/5M6is6gu3Vo1uVqyaNWf/sXfKyI0l240iwX9wpm6HE/Tg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script src="//cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.11.3/b-2.0.1/b-html5-2.0.1/b-print-2.0.1/fh-3.2.0/datatables.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/fixedheader/3.2.0/js/dataTables.fixedHeader.min.js"></script>
 
-</head>
+    </head>
 
 
 <body>
