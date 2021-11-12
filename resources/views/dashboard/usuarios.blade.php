26a809d0cd2afd4a9a5da887bf330a3c81d50822
Lucas Solderagit config --global user.name Lucas <lucas.s.soldera2004@gmail.com>
2021-11-12 01:07:12 -0300

    Rascunho de ínicio de filtro

--- resources/views/dashboard/usuarios.blade.php
+++ resources/views/dashboard/usuarios.blade.php
@@ -733,6 +733,11 @@
                 $('#logisticaPrivilegio').val(log);
             }
 
+            $('#example tfoot th').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" class="form-control" style="width:100%" placeholder="Pesquisar '+title+'" />' );
+    } );
+
             var table_usuario = $('#tb_usuario').DataTable({
                 paging: true,
                 searching: false,
@@ -770,53 +775,20 @@
                     'csvHtml5',
                     'pdfHtml5'
                 ],
-            initComplete: function () {
-            var api = this.api();
+                initComplete: function () {
+            // Apply the search
+            this.api().columns().every( function () {
+                var that = this;
 
-            // For each column
-            api
-                .columns()
-                .eq(0)
-                .each(function (colIdx) {
-                    // Set the header cell to contain the input element
-                    var cell = $('.filters th').eq(
-                        $(api.column(colIdx).header()).index()
-                    );
-                    var title = $(cell).text();
-                    $(cell).html('<input type="text" placeholder="' + title + '" />');
-
-                    // On every keypress in this input
-                    $(
-                        'input',
-                        $('.filters th').eq($(api.column(colIdx).header()).index())
-                    )
-                        .off('keyup change')
-                        .on('keyup change', function (e) {
-                            e.stopPropagation();
-
-                            // Get the search value
-                            $(this).attr('title', $(this).val());
-                            var regexr = '({search})'; //$(this).parents('th').find('select').val();
-
-                            var cursorPosition = this.selectionStart;
-                            // Search the column for that value
-                            api
-                                .column(colIdx)
-                                .search(
-                                    this.value != ''
-                                        ? regexr.replace('{search}', '(((' + this.value + ')))')
-                                        : '',
-                                    this.value != '',
-                                    this.value == ''
-                                )
-                                .draw();
-
-                            $(this)
-                                .focus()[0]
-                                .setSelectionRange(cursorPosition, cursorPosition);
-                        });
-                });
-        },
+                $( 'input', this.footer() ).on( 'keyup change clear', function () {
+                    if ( that.search() !== this.value ) {
+                        that
+                            .search( this.value )
+                            .draw();
+                    }
+                } );
+            } );
+        }
             });
             var table_cargo = $('#tb_cargo').DataTable({
                 paging: true,
